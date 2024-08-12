7be3883868b8a374ae87dfbe84b7a15771b6a9fa
unknown <maheedhar gowd>
2024-08-12 10:43:49 +0530

    final update

--- python.py
+++ python.py
@@ -8,12 +8,11 @@ english_to_morse = {
     '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', 
     '7': '--...', '8': '---..', '9': '----.',
     # special characters
-    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '(': '-.--.', '/': '-..-.',
+    '~':'.-.-..','.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '(': '-.--.', '/': '-..-.',
     ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', 
-    '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': ' '
+    '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', '#':'.--.--...',' ': ' ','%':'.----.-'
 }
 morse_to_english = { 
-
 }
 values = list(english_to_morse.keys())
 keys   = list(english_to_morse.values())
@@ -21,45 +20,46 @@ keys   = list(english_to_morse.values())
 for key ,value in zip(keys,values) :
     morse_to_english[key]=value
 
-user_input = input('enter an english string :')   
-
-
 def english_to_morsecode_translator(text):
-    output = ''
-    if len(text) == 0:
-        print('given string is empty .')
-
-    else :
-
-        for i in text:
-            
-            output += english_to_morse[i.capitalize()] + '   '
-        
-        return output.strip()
-
+    output = ''  
+    english_words = []
+    english_letters =''
+    
+    english_words.append(text.split())
+    
+    for i in english_words[0] :
+        english_letters += i+' '
+    for i in english_letters:
+        output += english_to_morse[i.capitalize()]+'   '
+    return output.strip()
+ 
 def morse_english_translator (text):
     output=''
-    words = text.split('       ')
-    
+    morse_words = text.split('       ')
+    morse_letters =[]
+    for i in range(len(morse_words)):
 
-    letters =[]
-    for i in range(len(words)):
-        letters.append(words[i].split('   ')+[' '])
-    
-    
-    for i in letters :
+        morse_letters.append(morse_words[i].split('   ')+[' '])
+    for i in morse_letters :
         for j in i:
             output+=morse_to_english[j]
-    
-    return output
+    return output.strip()
 
 
-e_m_output=english_to_morsecode_translator(user_input)
-print(e_m_output)
-m_e_output = morse_english_translator (e_m_output) 
-print('converting the above morse to english :',m_e_output)
+user_input = input('enter an english string :') 
+
+def result(text):
+    if len(text.strip()) == 0 :
+        print('given string is empty')
+    else : 
+             
+        e_m_output=english_to_morsecode_translator(text)
+        print('english to morse code :  ',e_m_output)
+        m_e_output = morse_english_translator (e_m_output) 
+        print('converting the above morse to english :',m_e_output)
 
 
+result(user_input)
 
 
 
